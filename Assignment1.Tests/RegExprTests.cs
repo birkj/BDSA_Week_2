2fa6c174502cb700d52e68f9a1c6fdc85e4daf75
Markusgp <markusgp@live.dk>
2021-09-13 10:18:46 +0200

    Added resolution, and testing (#4)
    Added regex and method for resolution. Although the test fails when converting between IEnumerable<string> to string. WIP.

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
 namespace Assignment1.Tests
@@ -7,6 +8,28 @@ namespace Assignment1.Tests
     public class RegExprTests
     {
         [Fact]
+
+        public void regexOutput_matches_inputResolution() {
+
+            //Arrange
+            IEnumerable<string> resolutions = new string[]{"1920x1080"};
+
+    
+            string expected = @"(1920, 1080)
+                    (1024, 768)
+                    (800, 600)
+                    (640, 480)
+                    (320, 200)
+                    (320, 240)
+                    (800, 600)
+                    (1280, 960)";
+
+            //Act
+            var actualOutput = RegExpr.Resolution(resolutions);
+
+            //Assert
+            Assert.Equal(expected, actualOutput);
+          
         public void Lines_With_One_Word()
         {
             // Arrange
