43dd21ae1e6c7d84bb8402bcc2c4cb317e83f5e6
Markusgp <markusgp@live.dk>
2021-09-13 10:45:07 +0200

    Regex input & expected input

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -12,9 +12,9 @@ namespace Assignment1.Tests
         {
 
             // Arrange
-            IEnumerable<string> resolutions = new List<string> { "1920x1080" };
+            IEnumerable<string> resolutions = new List<string> { "1920x1080", "1024x768", "800x600", "640x480","320x200", "320x240", "800x600","1280x960" };
 
-            IEnumerable<(int, int)> expected = new List<(int, int)> { (1024, 768), (800, 768), (640, 480), (320, 200), (320, 240), (800, 600), (1280, 960) };
+            IEnumerable<(int, int)> expected = new List<(int, int)> { (1920, 1080), (1024, 768), (800, 600), (640, 480), (320, 200), (320, 240), (800, 600), (1280, 960) };
 
             // Act
             var actualOutput = RegExpr.Resolution(resolutions);
